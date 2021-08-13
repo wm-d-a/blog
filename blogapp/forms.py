4be0e27fdd22d55e3b42e2fcbab6a95beb5fc528
wm-d-a <damirnizamov7@mail.ru>
2021-08-13 15:35:37 +0500

    added platform to authorization

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.contrib.auth.models import User
 from .models import Post
 
 
@@ -7,3 +7,19 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('rubric', 'title', 'text', 'user')
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        if not User.objects.filter(username=username).exist():
+            raise forms.ValidationError(f'Пользователь с логином {username} не найден в системе')
+        user = user.objects.filter(username=username).first()
